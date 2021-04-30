f4104c143995897a1d3577b4718a806920eae121
Taylor Dye <dyetaylor@icloud.com>
2021-04-29 20:50:41 -0400

    purged some CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: [
+      '/templates/barry_home.html'
+    ],
+    
+    enabled: true,
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
